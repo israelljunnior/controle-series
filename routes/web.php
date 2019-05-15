3dfc56fa58ce4468cf3ef17b5127efc502cfc98b
Israel Gomes de Sena Junior <israelljunnior15987426@hotmail.com>
2019-05-15 16:00:16 -0300

    update rotas

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', 'SeriesController@index')
+    ->name('listar_series');
+
 Route::get('/series', 'SeriesController@index')
     ->name('listar_series');
 
